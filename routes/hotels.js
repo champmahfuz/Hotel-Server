dce68fca1d62d66b12da39027596d7dbed5259dc
Mahfuz Rahman <mahfuzrahman517@gmail.com>
2023-01-15 18:47:15 +0600

    added hotel

---
+++ routes/hotels.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js';
+import Hotel from '../models/Hotel.js';
+
+const router = express.Router();
+
+//create
+
+router.post('/', createHotel);
+
+//update
+
+router.put('/:id', updateHotel);
+
+//delete
+router.delete('/:id', deleteHotel);
+
+//get
+router.get('/:id', getHotel);
+
+//grt all
+router.get('/', getHotels);
+
+export default router;
---
+++ routes/users.js
@@ -0,0 +1,20 @@
+/* import express from 'express';
+import { deleteUser, getUser, getUsers, updateUser } from '../controllers/user';
+import { verifyToken } from '../utlis/varifyToken';
+
+const router = express.Router();
+
+//update
+
+router.put('/:id', updateUser);
+
+//delete
+router.delete('/:id', deleteUser);
+
+//get
+router.get('/:id', getUser);
+
+//grt all
+router.get('/', getUsers);
+
+export default router; */
